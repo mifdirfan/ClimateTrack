865a9fa459d6ae00e62d3d313b19654f671e93e7
hirzi-hhh <hirziirsyad2@gmail.com>
2025-06-10 13:48:13 +0900

    update homepage and app icon and etc front end

--- FrontEnd/app/(tabs)/index.tsx
+++ FrontEnd/app/(tabs)/index.tsx
@@ -1,3 +1,5 @@
+// HOMEPAGE!!
+
 import React, { useState } from 'react';
 import { View, Text, TextInput, FlatList, TouchableOpacity, Image } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
