8d2656cb7ea09eabeadd9dab66cb7cbc69e713c6
Mifdzal Irfan <mifdzalirfan23@gmail.com>
2025-06-10 15:36:38 +0900

    complete scenario and send succes signal

--- backend/src/main/java/com/ClimateTrack/backend/Controller/ReportController.java
+++ backend/src/main/java/com/ClimateTrack/backend/Controller/ReportController.java
@@ -3,6 +3,7 @@ package com.ClimateTrack.backend.Controller;
 import com.ClimateTrack.backend.Service.ReportService;
 import com.ClimateTrack.backend.dto.ReportRequestDto;
 import com.ClimateTrack.backend.dto.ReportResponseDto;
+import com.ClimateTrack.backend.dto.SuccessResponse;
 import lombok.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,8 +18,9 @@ public class ReportController {
     private final ReportService reportService;
 
     @PostMapping
-    public ResponseEntity<ReportResponseDto> submitReport(@RequestBody ReportRequestDto dto) {
-        return ResponseEntity.ok(reportService.saveReport(dto));
+    public ResponseEntity<SuccessResponse> submitReport(@RequestBody ReportRequestDto dto) {
+        reportService.saveReport(dto);
+        return ResponseEntity.ok(new SuccessResponse(true, "Report saved successfully."));
     }
 
     @GetMapping
