7d8739c5997e395c1256e33caa603a77d12dbd20
hirzi-hhh <hirziirsyad2@gmail.com>
2025-10-26 04:58:39 +0900

    community page, feed and post (to check again)

--- FrontEnd/constants/ApiConfig.ts
+++
@@ -1,5 +0,0 @@
-// Replace with your computer's current local IP address.
-// Make sure your mobile device and computer are on the same Wi-Fi network.
-const API_BASE_URL = 'http://192.168.219.104:8080';
-
-export default API_BASE_URL;
